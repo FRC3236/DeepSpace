d19c67a1afc2af6c586dd79f2d364b836584a670
Tri Force <frcteam3236@gmail.com>
2019-01-14 20:26:37 -0500

    Added basic vision functions, confirmed GetContourPairs() functionality

--- src/main/java/frc/robot/commands/TeleopDefault.java
+++ src/main/java/frc/robot/commands/TeleopDefault.java
@@ -12,44 +12,46 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.CommandBase;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
-import frc.robot.CommandBase;
 
 public class TeleopDefault extends Command {
-  public TeleopDefault() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
-    // Delete me
-  }
+	public TeleopDefault() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		// Delete me
+	}
 
-  // Called just before this Command runs the first time
-  @Override
-  protected void initialize() {
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
 
-  }
+	}
 
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
-    double forwardSpeed, lateralSpeed; 
-    forwardSpeed = CommandBase.controls.Driver.getY(Hand.kLeft);
-    lateralSpeed = CommandBase.controls.Driver.getX(Hand.kRight);
-    CommandBase.drivetrain.Drive(lateralSpeed - forwardSpeed, lateralSpeed + forwardSpeed);
-  }
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		double forwardSpeed, lateralSpeed; 
+		forwardSpeed = CommandBase.controls.Driver.getY(Hand.kLeft);
+		lateralSpeed = CommandBase.controls.Driver.getX(Hand.kRight);
+		
+		CommandBase.visionRocket.GetContourPairs();
 
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return false;
-  }
+		CommandBase.drivetrain.Drive(lateralSpeed - forwardSpeed, lateralSpeed + forwardSpeed);
+	}
 
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-  }
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-  }
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
 }
